a7a659b17af231363ef32e845028a4a58935f2d7
Adam Hutchings <55220104+adamhutchings@users.noreply.github.com>
2021-10-16 17:43:18 -0700

    Add cloud pulling (#81)
    * Add dummy startup print
    
    * Change run permissions on file
    
    * Open DB on cardinal startup
    
    * Don't reload (or start 2 processes)
    
    * Add pymongo req
    
    * Add dnspython
    
    * Black reformatting
    
    * Ignore password
    
    * Remove id from all the json data
    
    * Add data request (UNABLE TO BE TESTED)
    
    * Revert debug to False
    
    * Black
    
    * Add working connection string
    
    * Add new database name, and get pulling working
    
    * Remove comment line
    
    Co-authored-by: jakeroggenbuck <jakeroggenbuck2@gmail.com>

--- cardinal/api/cardinal_data_request.py
+++ cardinal/api/cardinal_data_request.py
@@ -1,4 +1,5 @@
 """All the "main" cardinal functionality should go here."""
+from typing import List, Dict
 
 COLLECTIONS = [
     "obj_pit_collection",
@@ -12,39 +13,59 @@ COLLECTIONS = [
     "calc_pickability",
     "calc_tba_tim",
     "calc_spr",
+    "raw_subj_pit",
+    # different names
+    "predicted_team",
+    "subj_team",
+    "obj_tim",
+    "tba_team",
+    "tba_tim",
+    "predicted_aim",
+    "pickability",
+    "tba_cache",
+    "obj_team",
+    "subj_aim",
+    "raw_obj_pit",
+    "unconsolidated_obj_tim",
 ]
 
 
-CONNECTION_STR = (
-    'mongodb+srv://server:{}@scouting-system-3das1.gcp.mongodb.net/test?retryWrites=true&w=majority'
-)
+CONNECTION_STR = "mongodb+srv://cardinal-web-server:{}@scouting-system-3das1.gcp.mongodb.net/test?authSource=admin&replicaSet=scouting-system-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true"
 PORT = 27017
 
 
 CLIENT = None
-
-
 DB = None
 
 
-def get_unsent_docs(collection_name: str):
-    if collection_name in COLLECTIONS:
-        # TODO: get documents from that collection
-        # filter through and only return new documents
-        return f"Requested data from {collection_name}"
+def serialize_documents(docs) -> List[Dict]:
+    """Remove the '_id' from each document"""
+    docs = list(docs)
+    for doc in docs:
+        doc.pop("_id", None)
 
-    else:
+    return docs
+
+
+def get_unsent_docs(collection_name: str):
+    if collection_name not in COLLECTIONS:
         return f"The collection '{collection_name}' does not exist. \
 To get a list of supported collections, look at /api/supported-collections/"
+
+    # TODO - Actually get unsent data, not just all of the data
+    # available. This will be called the "Data Delta" feature
+    collections = DB.list_collection_names()
+
+    return serialize_documents(DB[collection_name].find())
 
 
 def get_match_schedule(comp_code: str):
     # TODO: return the actual match schedule
-    with open('cardinal/api/hardcoded_test_data/match_schedule.csv') as file:
+    with open("cardinal/api/hardcoded_test_data/match_schedule.csv") as file:
         return file.read()
 
 
 def get_teams_list(comp_code: str):
     # TODO: Get the actual teams list
-    with open('cardinal/api/hardcoded_test_data/team_list.csv') as file:
+    with open("cardinal/api/hardcoded_test_data/team_list.csv") as file:
         return file.read()
